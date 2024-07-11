6c54bc20698c9871674258bc6dfbf0efa945e1c0
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-11 15:57:33 +0300

    ...

---
+++ Services/Discount/TakeAway.Discount/Context/DiscountContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TakeAway.Discount.Entites;
+
+namespace TakeAway.Discount.Context
+{
+    public class DiscountContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial Catalog=TakeAwayDiscountDb;integrated Security=true");
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
